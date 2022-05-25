afca15ea6346ae97291aa411071bd787a1ea0e69
NassimAm <nassimam2003@gmail.com>
2022-05-25 23:20:12 +0100

    Initial Commit

--- src/main/java/com/example/tppoo/MainController.java
+++ src/main/java/com/example/tppoo/MainController.java
@@ -3,7 +3,7 @@ package com.example.tppoo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
